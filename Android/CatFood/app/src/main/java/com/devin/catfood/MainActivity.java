b0a5dbf6146a13df3e364b5f9845c5c4ae04dfbe
itsnotafake <itsnotafake@gmail.com>
2016-07-04 15:52:38 -0700

    Added some buttons to the main xml page

--- Android/CatFood/app/src/main/java/com/devin/catfood/MainActivity.java
+++ Android/CatFood/app/src/main/java/com/devin/catfood/MainActivity.java
@@ -2,6 +2,9 @@ package com.devin.catfood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                checkBox.setEnabled(false);
+                Log.w("dg", "checkBox pressed");
+            }
+        });
+
+        final CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                checkBox2.setEnabled(false);
+                Log.w("dg", "checkBox2 pressed");
+            }
+        });
     }
 }
